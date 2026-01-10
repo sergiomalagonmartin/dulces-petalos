7653ec7343ed92071bd6ef882430b56be71c018f
Sergio Malagón <smalagonmartin@gmail.com>
2026-01-10 16:32:47 +0100

    feat: added breadcrumbs component

---
+++ components/breadcrumbs.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from 'react';
+import { AngleRight } from './icons/angle-right';
+import Link from 'next/link';
+
+export function Breadcrumbs({ children }: { children: ReactNode[] }) {
+  const breadcrumbs = [];
+
+  for (let i = 0; i < children.length; ++i) {
+    const child = children[i];
+
+    breadcrumbs.push(child);
+
+    if (i < children.length - 1) breadcrumbs.push(<AngleRight key='breadcrumb-separator' />);
+  }
+
+  return <div className='flex gap-3xs'>{breadcrumbs}</div>;
+}
+
+export function Breadcrumb({ title, href }: { title: string; href?: string }) {
+  if (href) {
+    return (
+      <Link href={href} className='b-1 text-neutral-800'>
+        {title}
+      </Link>
+    );
+  }
+
+  return <p className='b-1 text-neutral-800'>{title}</p>;
+}
