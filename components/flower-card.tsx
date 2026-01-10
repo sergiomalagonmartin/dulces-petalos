69a05b615e50dc369d027f29f65587616359d816
Sergio Malagón <smalagonmartin@gmail.com>
2026-01-10 17:44:02 +0100

    chore: updated props

--- components/flower-card.tsx
+++ components/flower-card.tsx
@@ -3,26 +3,29 @@ import { Pill } from './pill';
 import Link from 'next/link';
 import { Arrow } from './icons/arrow';
 import { Tag } from './tag';
+import type { Flower } from '@/lib/types';
 
-interface FlowerCardProps {
-  id: string;
-  title: string;
-  binominalName: string;
-  price: number;
-  image: string;
-  isNew?: boolean;
-}
+type FlowerCardProps = Flower & { isNew: boolean; className?: string };
 
-export function FlowerCard({ id, title, binominalName, price, image, isNew }: FlowerCardProps) {
+export function FlowerCard({ id, name, binomialName, price, imgUrl, isNew, className = '' }: FlowerCardProps) {
   return (
-    <div className='flex flex-col w-full max-w-[384px] rounded-[32px] p-xs gap-xs bg-neutral-100 shadow-300'>
+    <div
+      className={`flex flex-col w-full max-w-[384px] rounded-[32px] p-xs gap-xs bg-neutral-100 shadow-300 ${className}`}
+    >
       <div className='flex flex-col gap-2xs w-fit'>
-        <h4 className='text-neutral-1000'>{title}</h4>
-        <p className='b-1 text-neutral-800'>{binominalName}</p>
+        <h4 className='text-neutral-1000'>{name}</h4>
+        <p className='b-1 text-neutral-800'>{binomialName}</p>
       </div>
 
       <div className='relative h-75 w-full'>
-        <Image src={image} alt={title} fill className='object-center object-cover rounded-xs' />
+        <Image
+          src={imgUrl}
+          alt={name}
+          fill
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          loading='eager'
+          className='object-center object-cover rounded-xs'
+        />
 
         {isNew && <Tag content='Nuevo' variant='success' className='absolute right-2.5 top-2.5' />}
 
